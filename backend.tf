65e226f9b4fb2816bd38549dfc2c43ab2a525266
Charles Hodge <themercilessviolator-azdo@yahoo.com>
2021-12-12 18:11:17 -0500

    Updated YAML and state config.

--- backend.tf
+++ backend.tf
@@ -9,12 +9,11 @@
 # }
 
 
-terraform {  
-    backend "s3" {
-        bucket         = "aws-s3-bucket-test-state"    # the name of the S3 bucket that was created
-        encrypt        = true
-        key            = "terraform.tfstate"    # the path to the terraform.tfstate file stored inside the bucket
-        region         = "us-east-1"         # the location of the bucket
-        dynamodb_table = "aws-locks" # the name of the table to store the lock
-    }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "aws-s3-bucket-test-state" # the name of the S3 bucket that was created
+#     key            = "terraform-azdo.tfstate"       # the path to the terraform.tfstate file stored inside the bucket
+#     region         = "us-east-1"                # the location of the bucket
+#     dynamodb_table = "s3-tfstate-lock"          # the name of the table to store the lock
+#   }
+# }
--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
   # access_key = var.aws_access_key
   # secret_key = var.aws_secret_key
 }
